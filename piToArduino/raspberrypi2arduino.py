1434dd8c88c5526f274ffadb09743e593f614f57
RkBass <42913004+RkBass@users.noreply.github.com>
2019-03-27 14:01:32 -0700

    Clean Files
    Mike Cheng-Godinez
    Rick B

--- piToArduino/raspberrypi2arduino.py
+++ piToArduino/raspberrypi2arduino.py
@@ -1,29 +1,14 @@
 import serial
 import time
 ser = serial.Serial('/dev/ttyACM0', 9600)
-while 1:
-    '''bad code
-    for i in range(5):
-        ser.write(b'0')
-        time.sleep(1)
-    for i in range(5):
-        ser.write(b'1')
-        time.sleep(1)
-        '''
+
+def transmit(i):
+    #print( bytes( chr(i), 'utf-8') )
+    ser.write( bytes( chr(i), 'utf-8') )
+    
+while 1:#forever loop
+    #testing
     for i in range (11):#0-10
-        write_me = chr(i)#convert int to string
-        '''bad code
-        write_me = write_me.split('0')
-        write_me.insert(1,str(i))
-        write_me = "".join(write_me)
-        if write_me == '10':#get next value after '9' not 49->48
-            write_me = ':'#value after '9'
-        print(write_me)#check
-        '''
-        write_me = bytes(write_me, 'utf-8')
-        ser.write(write_me)
+        transmit(i)
         time.sleep(1)
-    #for i in range(10):
-        #ser.write(b'0')
-        #time.sleep(100)
 

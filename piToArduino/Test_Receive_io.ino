3b6cf4372321ec003cf68ef45d77dc53e2a64edd
RkBass <42913004+RkBass@users.noreply.github.com>
2019-03-27 15:24:32 -0700

    Signal Priority

--- piToArduino/Test_Receive_io.ino
+++ piToArduino/Test_Receive_io.ino
@@ -1,35 +1,51 @@
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <RF24_config.h>
+#include <SPI.h>
+
 int r=1;
+int input;
+int delayCount = 5;
+RF24 radio(7,8);
+const byte address[6] = "00001";
+
 void setup(){
   Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe();
+  
 }
-int counton=0;
-int countoff=0;
 void loop()
-{
-  if(Serial.available())
-  {
-    //r=(Serial.read()-'0');
-    //Serial.println(r);
-    //if(r == '1')//{
-     int r = Serial.read();
-     if(r==48)
-      {
-        //++counton;
-        Serial.print("NOT ON ANYMORE ");
-        Serial.println(r);
-        digitalWrite(12, HIGH);
-        delay(500);
-        //digitalWrite(12, LOW);
-        //delay(500);
+{ 
+  //priority code
+  while(1){
+    radio.startListening();
+    if(radio.available()){//listen to controller
+      while(radio.available()){
+        r = radio.read();
+        if(r != 0){
+          //taking controller input that is not 0
+          Serial.println(r);
+          delayCount = 5;
+        }
       }
-      else//(Serial.read()==49)
-      {
-        //++countoff;
-        Serial.print("NOT OFF ANYMORE ");
+    }
+    //take pi input, if controller is available or not
+    if(Serial.available())
+    {
+      if(delayCount > 0){
+        //count down delay
+        delay(3);
+        delayCount = delayCount - 1;
+      }else{
+        //taking the pi input
+        r = Serial.read()
         Serial.println(r);
-        digitalWrite(12, LOW);
         delay(500);
       }
+    }
   }
+  //0-10 conditions below
+  
 }
 
